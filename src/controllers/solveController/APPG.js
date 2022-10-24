f85bc45fb8b486a850028cb93ed07c6c550a5e46
Lucca Medeiros <silvaluccam@gmail.com>
2022-10-24 20:45:04 -0300

    Ao rodar o lizard, vimos que a funcionalidade solve APPG possuía um cyclomatic complexity > 15. Essa métrica de software mede a quantidade de caminhos de execução independentes a partir de um código fonte, ou seja, é uma forma de medir a complexidade de um software. Antes da refatoração essa funcionalidade consultava em um config de correção do teste a pontuação correspondente a cada afirmativa e através de um switch pontuava cada questão. Isso é ruim pois resulta em uma grande quantidade de caminhos de execuções possíveis, deixando a funcionalidade como um todo complexa. Haja vista essa situação a refatoração consistiu em substituir essa abordagem por um mapping, que mapeia as respostas do teste no objeto config de correção, executando a tarefa de um jeito menos complexo que o anterior, e consequentemente deixando o código mais legível e eficiente.

--- src/controllers/solveController/APPG.js
+++ src/controllers/solveController/APPG.js
@@ -3,76 +3,14 @@ const configAPPG = require('../config/APPG');
 module.exports = {
 
     async solve(data) {
-        let A = 0, B = 0, C = 0, D = 0, E = 0, F = 0, G = 0, H = 0, I = 0, J = 0;
-        let K = 0, L = 0, M = 0, N = 0, O = 0, P = 0, Q = 0, R = 0, S = 0, T = 0;
+        const obj = {};
         for (const [key, value] of Object.entries(data)) {
-            switch (configAPPG[key][value]) {
-
-                case 'A':
-                    A += 1;
-                    break;
-                case 'B':
-                    B += 1;
-                    break;
-                case 'C':
-                    C += 1;
-                    break;
-                case 'D':
-                    D += 1;
-                    break;
-                case 'E':
-                    E += 1;
-                    break;
-                case 'F':
-                    F += 1;
-                    break;
-                case 'G':
-                    G += 1;
-                    break;
-                case 'H':
-                    H += 1;
-                    break;
-                case 'I':
-                    I += 1;
-                    break;
-                case 'J':
-                    J += 1;
-                    break;
-                case 'K':
-                    K += 1;
-                    break;
-                case 'L':
-                    L += 1;
-                    break;
-                case 'M':
-                    M += 1;
-                    break;
-                case 'N':
-                    N += 1;
-                    break;
-                case 'O':
-                    O += 1;
-                    break;
-                case 'P':
-                    P += 1;
-                    break;
-                case 'Q':
-                    Q += 1;
-                    break;
-                case 'R':
-                    R += 1;
-                    break;
-                case 'S':
-                    S += 1;
-                    break;
-                case 'T':
-                    T += 1;
-                    break;
-                default:
-                    break;
+            if (!obj[configAPPG[key][value]]) {
+                obj[configAPPG[key][value]] = 0;
             }
+            obj[configAPPG[key][value]] += 1;
         }
-        return { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T };
+        return obj;
     },
 
 }
