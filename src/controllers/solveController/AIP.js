583bd01f0eec5733d41e23c5fa0cdc270f329e2e
Lucca Medeiros <silvaluccam@gmail.com>
2022-10-24 22:01:34 -0300

    Ao rodar o lizard, vimos que a funcionalidade solve AIP possuía um cyclomatic complexity > 15. Essa métrica de software mede a quantidade de caminhos de execução independentes a partir de um código fonte, ou seja, é uma forma de medir a complexidade de um software. Antes da refatoração essa funcionalidade consultava em um config de correção do teste a pontuação correspondente a cada afirmativa e através de um switch pontuava cada questão. Isso é ruim pois resulta em uma grande quantidade de caminhos de execuções possíveis, deixando a funcionalidade como um todo complexa. Haja vista essa situação a refatoração consistiu em substituir essa abordagem por um mapping, que mapeia as respostas do teste no objeto config de correção, executando a tarefa de um jeito menos complexo que o anterior, e consequentemente deixando o código mais legível e eficiente.

--- src/controllers/solveController/AIP.js
+++ src/controllers/solveController/AIP.js
@@ -3,76 +3,24 @@ const configAIP = require('../config/AIP');
 module.exports = {
 
     async solve(data) {
-        let A = 0, B = 0, C = 0, D = 0, E = 0, F = 0, G = 0, H = 0, I = 0, J = 0;
+        const obj = {};
         for (const [key, value] of Object.entries(data)) {
-            switch (configAIP[key][value]) {
-
-                case 'A':
-                    A += 1;
-                    break;
-                case 'B':
-                    B += 1;
-                    break;
-                case 'C':
-                    C += 1;
-                    break;
-                case 'D':
-                    D += 1;
-                    break;
-                case 'E':
-                    E += 1;
-                    break;
-                case 'F':
-                    F += 1;
-                    break;
-                case 'G':
-                    G += 1;
-                    break;
-                case 'H':
-                    H += 1;
-                    break;
-                case 'I':
-                    I += 1;
-                    break;
-                case 'J':
-                    J += 1;
-                    break;
-
-                case 'A2':
-                    A += 0.5;
-                    break;
-                case 'B2':
-                    B += 0.5;
-                    break;
-                case 'C2':
-                    C += 0.5;
-                    break;
-                case 'D2':
-                    D += 0.5;
-                    break;
-                case 'E2':
-                    E += 0.5;
-                    break;
-                case 'F2':
-                    F += 0.5;
-                    break;
-                case 'G2':
-                    G += 0.5;
-                    break;
-                case 'H2':
-                    H += 0.5;
-                    break;
-                case 'I2':
-                    I += 0.5;
-                    break;
-                case 'J2':
-                    J += 0.5;
-                    break;
-                default:
-                    break;
+            if (!configAIP[key][value]) {
+                continue;
+            }
+            const letter = configAIP[key][value].slice(0, 1);
+            const variation = configAIP[key][value].slice(1, 2);
+            if (!obj[letter]) {
+                obj[letter] = 0;
+            }
+            if (variation == '') {
+                obj[letter] += 1;
+            }
+            if (variation == '2') {
+                obj[letter] += 0.5;
             }
         }
-        return { A, B, C, D, E, F, G, H, I, J };
+        return obj;
     },
 
 }
